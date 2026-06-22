781b3907a532955333f39547faed24a41ca621e5
Tanishk Khare <tanishk16012004@gmail.com>
2026-06-22 11:30:24 +0530

    go basic programs

--- go/fundamentals/main.go
+++ go/fundamentals/main.go
@@ -3,5 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world")
+
+	var programName = "Get a girlfriend program"
+	const totalTickets = 50
+	var remainingTickets uint = 50
+
+
+	//as we have provided the uint type explicitly so remainingTickets cannot be negative at all
+	// remainingTickets = -2
+
+
+	fmt.Println("Welcome to our", programName)
+	fmt.Printf("We have limited number of ticket : %v\n", totalTickets)
+	fmt.Println("Available tickets:", remainingTickets)
+
+	//a new user comes must have a username and usermail
+
+	var userName string
+	var userTickets int
+
+	userName = "Tanishk"
+	userTickets = 50
+
+	fmt.Println(userName)
+	fmt.Println(userTickets)
+
+	//supppose the user has booked some tickets
+
+	fmt.Printf("%T is the type of username and %T is the type of tickets\n" , userName , userTickets)
+	fmt.Printf("%v has booked %v tickets\n", userName, userTickets)
+
 }
