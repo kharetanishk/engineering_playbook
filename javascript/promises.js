455100d3929db1d83881944e896b3d4d453ee3e2
Tanishk Khare <tanishk16012004@gmail.com>
2026-07-18 02:46:38 +0530

    promises start

---
+++ javascript/promises.js
@@ -0,0 +1,16 @@
+function check(n){
+   return n === "tanishk";
+}
+
+const p = new Promise((res , rej)=>{
+   check("tanishk") ? res("promise resolved") : rej("promise rejected")
+})
+
+fetchdata = async()=>{
+    try{
+        console.log(await p)
+    }catch(e){
+        console.log(e)
+    }
+}
+fetchdata()
