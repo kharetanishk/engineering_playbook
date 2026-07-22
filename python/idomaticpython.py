5cd81183c1dde1645cd6729616f63dc57bc11706
Tanishk Khare <tanishk16012004@gmail.com>
2026-07-22 19:59:07 +0530

    idomatic pythoon

---
+++ python/idomaticpython.py
@@ -0,0 +1,87 @@
+#old way
+
+num1 = [1,2,3,4]
+twice_num1 = []
+for n in num1:
+    twice_num1.append(n * 2)
+print(twice_num1)
+
+#pythonic way
+
+twice_num1_new = [n * 2 for n in num1 ]
+print(twice_num1_new)
+
+#adding a condition
+even = [n for n in num1 if n % 2 == 0]
+print(even)
+
+#strings
+names =["tanishk","shreya","omi"]
+caps = [n.upper() for n in names]
+print(caps)
+
+#dictionary comprehension
+
+#old
+squares = {}
+for x in range(5):
+    squares[x] = x*x
+print(squares)
+
+#pythonic way 
+squares_1 = {
+    x:x*x
+    for x in range(5)
+}
+print(squares_1)
+
+#questions
+# nums = [1,2,3,4,5] if these are nums 
+# write a list comprehension to produce [2,4,6,8,10]
+nums_2 = [1,2,3,4,5]
+list_1 = [x*2 for x in nums_2]
+print(list_1)
+
+#list comprehension to produce this [1,9,25]
+list_2 = [x*x for x in nums_2 if x % 2 != 0]
+print(list_2)
+# Q3
+
+# Given
+
+# names = ["tanu","john","alice"]
+
+# Produce
+
+# ["TANU","JOHN","ALICE"]
+
+# using a comprehension.
+
+names = ["tanu","john","alice"]
+capitalize = [c.upper() for c in names]
+print(capitalize)
+
+# Given
+
+# scores = {
+#     "math":90,
+#     "science":95
+# }
+
+# Write a dictionary comprehension that adds 10 to every score.
+scores = {
+     "math":90,
+     "science":95
+ }
+
+add_10 = {
+    x: scores[x]+10
+    for x in scores
+}
+print(add_10)
+
+add_10_2 = {
+    sub : score + 10
+    for sub , score in scores.items()
+}
+print(add_10_2)
