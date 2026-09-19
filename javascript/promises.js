aa356c398b16f92223bfa1d616115c4d3874f472
Tanishk Khare <tanishk16012004@gmail.com>
2026-09-19 23:54:29 +0530

    promisified reading a document

--- javascript/promises.js
+++ javascript/promises.js
@@ -18,3 +18,18 @@ fetchdata()
 
 console.log("hello world")
 
+
+// promisified readFile: manual wrapper
+const fs = require("node:fs");
+const { promisify } = require("node:util");
+
+const readFileP = (path) =>
+    new Promise((resolve, reject) =>
+        fs.readFile(path, "utf8", (err, data) => (err ? reject(err) : resolve(data)))
+    );
+
+// same thing via util.promisify
+const readFileAsync = promisify(fs.readFile);
+
+readFileP(__filename).then((d) => console.log("manual:", d.length));
+readFileAsync(__filename, "utf8").then((d) => console.log("promisify:", d.length));
